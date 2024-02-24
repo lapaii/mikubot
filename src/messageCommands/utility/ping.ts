daef7d4cd2f1489d1841f0c95e580a5e5f6d81ed
lapaii <lapaii@lapaii.dev>
2024-02-24 18:48:21 +0000

    move run to separate function

--- src/messageCommands/utility/ping.ts
+++ src/messageCommands/utility/ping.ts
@@ -5,22 +5,24 @@ export default {
 	name: 'ping',
 	description: 'gives ping of bot',
 	cooldown: 1000,
-	run: async ({ message }: { message: Message }) => {
-		const pingMessage = await message.reply({
-			content: 'pinging...'
-		});
-		
-		const { ws, rest } = await message.client.ping();
-	
-		await pingMessage.edit({ 
-			content: '',
-			embeds: [
-				{
-					title: 'ping',
-					description: `websocket: ${ws}ms\nrest: ${rest}ms`,
-					color: 60,
-				}
-			]
-		});
-	}
+	run
 } satisfies MessageCommand;
+
+async function run({ message }: { message: Message }): Promise<void> {
+	const pingMessage = await message.reply({
+		content: 'pinging...'
+	});
+	
+	const { ws, rest } = await message.client.ping();
+
+	await pingMessage.edit({ 
+		content: '',
+		embeds: [
+			{
+				title: 'ping',
+				description: `websocket: ${ws}ms\nrest: ${rest}ms`,
+				color: 60,
+			}
+		]
+	});
+}
