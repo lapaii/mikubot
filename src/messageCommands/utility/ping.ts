139e2c7f3e3ceac9d0e0243aaa5c07497b771c13
lapaii <lapaii@lapaii.dev>
2024-02-29 13:32:28 +0000

    config stuff !!!

--- src/messageCommands/utility/ping.ts
+++ src/messageCommands/utility/ping.ts
@@ -1,28 +1,28 @@
-import { MessageCommand } from "../../types/commands.js";
-import { Message } from "lilybird";
+import type { MessageCommand } from "../../types/commands.js";
+import type { Message } from "lilybird";
 
 export default {
-	name: 'ping',
-	description: 'gives ping of bot',
-	cooldown: 1000,
-	run
+    name: "ping",
+    description: "gives ping of bot",
+    cooldown: 1000,
+    run
 } satisfies MessageCommand;
 
 async function run({ message }: { message: Message }): Promise<void> {
-	const pingMessage = await message.reply({
-		content: 'pinging...'
-	});
-	
-	const { ws, rest } = await message.client.ping();
+    const pingMessage = await message.reply({
+        content: "pinging..."
+    });
 
-	await pingMessage.edit({ 
-		content: '',
-		embeds: [
-			{
-				title: 'ping',
-				description: `websocket: ${ws}ms\nrest: ${rest}ms`,
-				color: 60,
-			}
-		]
-	});
+    const { ws, rest } = await message.client.ping();
+
+    await pingMessage.edit({
+        content: "",
+        embeds: [
+            {
+                title: "ping",
+                description: `websocket: ${ws}ms\nrest: ${rest}ms`,
+                color: 60
+            }
+        ]
+    });
 }
